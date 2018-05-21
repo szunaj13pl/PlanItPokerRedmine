97631acee64910ad47386906a5535541ff0394f4
Janusz Ładecki <szunaj13pl@gmail.com>
2018-05-21 13:45:14 +0200

    master

--- download-script.js
+++
@@ -1,4 +0,0 @@
-//  get download url from DOM Element
-downloadURL = $('div.searchDiv>p:nth-child(4)>a:nth-child(2)').attr('href');
-window.open(downloadURL);   //  open new tab
-window.close();             //  close tab
---
+++ issues-script.js
@@ -0,0 +1,33 @@
+function addHref(element) {
+    if ($(element).find('a').length == 0) {
+
+        const issueNrRegex = /\#\d{1,4}/gm;
+
+        const self = $(element);
+        const html = self[0].innerHTML;
+
+        const issueNr = html.match(issueNrRegex);
+        const number = issueNr[0].replace(/\#/, '');
+
+        const redmineUrl = `https://redmine.3s.pl/issues/${number}`;
+        const newTitle = html.replace(issueNrRegex, `<a target="_blank" href="${redmineUrl}">${issueNr}</a>`);
+
+        self[0].innerHTML = newTitle;
+    }
+}
+
+$(function () {
+
+    window.setInterval(function () {
+
+        $('tbody td span[ng-bind="s.shownTitle"]').each((index, element) => {
+            addHref(element);
+        });
+
+        $('.story-text').each((index, element) => {
+            addHref(element);
+        });
+
+    }, 5000);
+
+});
